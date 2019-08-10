5988acd65e33f4a35cb56a5b8840ebe195fab781
Ryan Yogan <ryanyogan@gmail.com>
2019-08-10 13:31:25 -0700

    added in environments

--- lib/webhook_processor/application.ex
+++ lib/webhook_processor/application.ex
@@ -10,7 +10,7 @@ defmodule WebhookProcessor.Application do
       Plug.Cowboy.child_spec(
         schema: :http,
         plug: WebhookProcessor.Endpoint,
-        options: [port: 4001]
+        options: [port: Application.get_env(:webhook_processor, :port)]
       )
     ]
 
