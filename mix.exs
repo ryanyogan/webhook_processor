5ffaba16d0ad0b773776086ab751e17c4d81e45f
Ryan Yogan <ryanyogan@gmail.com>
2019-08-10 13:18:09 -0700

    added in poison and plug_cowboy

--- mix.exs
+++ mix.exs
@@ -11,19 +11,17 @@ defmodule WebhookProcessor.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :plug_cowboy],
       mod: {WebhookProcessor.Application, []}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:poison, "~> 3.1"}
     ]
   end
 end
